1c4b22919f8e6b92c3707c60f97c58101b9b51fe
김지환 <jihwan@gimjihwan-ui-MacBookPro.local>
2022-02-10 16:35:57 +0900

    setting and css compelte

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    Show Data
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data  
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt=""/>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data(){
+    return{
+      loading:true,
+      title: 'Global',
+      dataDate: '',
+      status: {},
+      countries: [],
+      loadingImage: require('../assets/sandclock.gif'),
+    }
+  },
+  methods:{
+    async fetchCovidData(){
+      const res = await fetch(`https://api.covid19api.com/summary`)
+      const data = await res.json()
+      return data
+    } 
+  },
+  created(){
+    const data = await this.fetchCovidData()
+    //console.log(data)
+    this.dateDate = data.dateDate
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = true
+  },
 }
 </script>
